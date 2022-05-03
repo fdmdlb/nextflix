a79c260e0cb4282fdf8cc96db5042cea0f51ece2
fdmdlb <99870318+fdmdlb@users.noreply.github.com>
2022-05-03 15:44:37 +0100

    correcting Data to data in filepath

--- cultural_interest.py
+++ cultural_interest.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-df_top10_number = pd.read_csv('./Data/6ko_top10_number.csv')
+df_top10_number = pd.read_csv('./data/6ko_top10_number.csv')
 print('Top 10 in number of media translated')
 print(df_top10number)
 print()
 
 print('Top 10 in number of minutes translated')
-df_top10_minutes = pd.read_csv('./Data/6ko_top10_minutes.csv')
+df_top10_minutes = pd.read_csv('./data/6ko_top10_minutes.csv')
 print(df_top10minutes)
 
