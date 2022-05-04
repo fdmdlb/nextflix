7defb50b134e30a8d75010ab2c1d950f27f09168
Joana Pires Coelho <63159680+Joana-Coelho@users.noreply.github.com>
2022-05-04 14:21:20 +0100

    create code for the second visualization

--- interface.py
+++ interface.py
@@ -62,3 +62,26 @@ ax_corr.set_yticklabels(list(publishers_corr.sort_values("audience_rating", asce
 
 #publishers_corr.sort_values("audience_rating", ascending=False)[1:6]
 st.pyplot(fig_corr)
+
+#####JOANA PART########################
+
+title_basic_table = pd.read_csv('./data/joana_visualization1.csv')
+title_basic = pd.read_csv('./data/joana_visualization2.csv')
+
+
+#first visualization
+
+sn.set(rc = {'figure.figsize':(15,8)})
+sn.set_style("white")  
+
+fig1_j,ax_j1=plt.subplots()
+
+ax_j1= sn.lineplot(data=title_basic, x="startYear", y="minutes").set(title= "Average length of movies/videos/tvmovies/short from 1874-2021", xlabel="Year", ylabel="Minutes")
+#plt.xlabel('Year', fontsize=20)
+#plt.ylabel('Minutes', fontsize=20);
+#plt.title("Average length of movies/videos/tvmovies/short from 1874-2021", fontsize=25)
+#plt.tick_params(axis='both', which='major', labelsize=15)
+
+
+st.pyplot(fig1_j)
+
