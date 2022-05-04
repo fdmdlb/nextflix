04806d5fe7ba7e4b1a5527ae8d4dbf05d3bcbd56
Joana Pires Coelho <63159680+Joana-Coelho@users.noreply.github.com>
2022-05-04 14:49:21 +0100

    updated order of visualizations and comments

--- interface.py
+++ interface.py
@@ -71,23 +71,6 @@ title_basic = pd.read_csv('./data/joana_visualization2.csv')
 
 #first visualization
 
-st.markdown('# Average movie* length evolution from 1874-2021')
-
-sn.set(rc = {'figure.figsize':(15,8)})
-sn.set_style("white")  
-fig1_j,ax_j1=plt.subplots()
-
-ax_j1= sn.lineplot(data=title_basic, x="startYear", y="minutes").set(xlabel="Year", ylabel="Minutes")
-plt.xlabel('Year', fontsize=20)
-plt.ylabel('Minutes', fontsize=20);
-#plt.title("Average length of movies/videos/tvmovies/short from 1874-2021", fontsize=25)
-plt.tick_params(axis='both', which='major', labelsize=15)
-
-st.pyplot(fig1_j)
-
-
-#second visualization
-
 st.markdown('# Individual length evolution in minutes from 1874-2021')
 
 sn.set(rc = {'figure.figsize':(15,8)})
@@ -105,3 +88,23 @@ plt.title('The length evolution of media products', fontsize=25)
 plt.tick_params(axis='both', which='major', labelsize=15)
 
 st.pyplot(fig2_j)
+
+
+#second visualization
+
+st.markdown('# Average movie* length evolution from 1874-2021')
+
+sn.set(rc = {'figure.figsize':(15,8)})
+sn.set_style("white")  
+fig1_j,ax_j1=plt.subplots()
+
+ax_j1= sn.lineplot(data=title_basic, x="startYear", y="minutes").set(xlabel="Year", ylabel="Minutes")
+plt.xlabel('Year', fontsize=20)
+plt.ylabel('Minutes', fontsize=20);
+#plt.title("Average length of movies/videos/tvmovies/short from 1874-2021", fontsize=25)
+plt.tick_params(axis='both', which='major', labelsize=15)
+
+st.pyplot(fig1_j)
+
+
+##joao part####
