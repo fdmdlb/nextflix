99bfcbdcf13753c8a084f1eb501707e8f2cdeae7
Joana Pires Coelho <63159680+Joana-Coelho@users.noreply.github.com>
2022-05-04 16:45:28 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -122,7 +122,7 @@ if selected==nav_list[3]:
     plt.tick_params(axis='both', which='major', labelsize=15)
 
     st.pyplot(fig2_j)
-    st.download_buuton(label="download chart",data=fig2_j)
+    st.download_button(label="download chart",data=fig2_j)
 
 if selected==nav_list[4]:
     #second visualization
