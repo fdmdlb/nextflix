4a13de4966474d4a8fed017b52c7cdc77fa3636c
Joana Pires Coelho <63159680+Joana-Coelho@users.noreply.github.com>
2022-05-04 14:33:37 +0100

    Update interface.py

--- interface.py
+++ interface.py
@@ -80,7 +80,7 @@ fig1_j,ax_j1=plt.subplots()
 ax_j1= sn.lineplot(data=title_basic, x="startYear", y="minutes").set(xlabel="Year", ylabel="Minutes")
 plt.xlabel('Year', fontsize=20)
 plt.ylabel('Minutes', fontsize=20);
-plt.title("Average length of movies/videos/tvmovies/short from 1874-2021", fontsize=25)
+#plt.title("Average length of movies/videos/tvmovies/short from 1874-2021", fontsize=25)
 plt.tick_params(axis='both', which='major', labelsize=15)
 
 
