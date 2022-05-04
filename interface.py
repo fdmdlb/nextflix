424dec792e1c1fe85b80e20bc7bbdfbeae574625
Joana Pires Coelho <63159680+Joana-Coelho@users.noreply.github.com>
2022-05-04 14:30:36 +0100

    update title

--- interface.py
+++ interface.py
@@ -71,7 +71,7 @@ title_basic = pd.read_csv('./data/joana_visualization2.csv')
 
 #first visualization
 
-st.markdown('# "Average movie* length,in minutes, evolution from 1874-2021')
+st.markdown('# Average movie* length evolution from 1874-2021')
 
 sn.set(rc = {'figure.figsize':(15,8)})
 sn.set_style("white")  
