518d32fad326183c0cc3d3ba3da9d6e4f6051ca4
FMrtnz <fab.mrtnz22@gmail.com>
2022-05-04 16:59:07 +0100

    solve error

--- cultural_interest.py
+++ cultural_interest.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import streamlit as st
 
 df_top10_number = pd.read_csv('./data/6ko_top10_number.csv')
-
-print('Top 10 in number of media translated')
-print(df_top10number)
-print()
-
 df_top10_minutes = pd.read_csv('./data/6ko_top10_minutes.csv')
-print('Top 10 in number of minutes translated')
-print(df_top10_minutes)
 
+#def display():
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Top 10 in number of media translated")
+    st.dataframe(df_top10_number)
+with col2:
+    st.header("Top 10 in number of minutes translated")
+    st.dataframe(df_top10_minutes)
