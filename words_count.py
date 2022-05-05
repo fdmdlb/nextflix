dec8714aca0748497122f4ad25a3835bbd830f19
FMrtnz <fab.mrtnz22@gmail.com>
2022-05-05 13:38:08 +0100

    change display words count

--- words_count.py
+++ words_count.py
@@ -12,6 +12,14 @@ def display_wordcloud():
 
     lemmentize_words_genres = pd.read_csv('./data/genres_X_lem.csv', converters={"genres": literal_eval, "X_lem": literal_eval})
 
+    st.markdown('## Most frequents words in Rotten vs Fresh movies')
+    list_fresh = word_freq[(word_freq.freq_total > 0.4)].sort_values(["rel_fresh"], ascending=[0]).head(10)["word"].values
+    list_rotten = word_freq[(word_freq.freq_total > 0.4)].sort_values(["rel_rot"], ascending=[0]).head(10)["word"].values
+    list_words = np.append(list_rotten, list_fresh)
+    #word_freq
+    fig = px.bar(word_freq[word_freq["word"].isin(list_words)].sort_values("General"), x='word', y=["Fresh", "Rotten"], title="Rotten vs Fresh movies", barmode="group")
+    st.plotly_chart(fig)
+
     col1, col2 = st.columns(2)
     with col1:
         # Set an input to choose region
@@ -25,11 +33,3 @@ def display_wordcloud():
         df_matrix = pd.DataFrame(text_matrix.toarray(), columns = vectorizer.get_feature_names_out())
         wordcloud = WordCloud(background_color="rgba(0,0,0,0)",width=700,height=500, min_font_size=10).generate_from_frequencies(df_matrix.sum())
         st.image(wordcloud.to_array())
-
-    st.markdown('## Most frequents words in Rotten vs Fresh movies')
-    list_fresh = word_freq[(word_freq.freq_total > 0.4)].sort_values(["rel_fresh"], ascending=[0]).head(10)["word"].values
-    list_rotten = word_freq[(word_freq.freq_total > 0.4)].sort_values(["rel_rot"], ascending=[0]).head(10)["word"].values
-    list_words = np.append(list_rotten, list_fresh)
-    #word_freq
-    fig = px.bar(word_freq[word_freq["word"].isin(list_words)].sort_values("General"), x='word', y=["Fresh", "Rotten"], title="Rotten vs Fresh movies", barmode="group")
-    st.plotly_chart(fig)
