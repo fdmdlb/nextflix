3c7cc8d12153c277ce7c6484d5b202ac25a89992
fdmdlb <99870318+fdmdlb@users.noreply.github.com>
2022-05-05 19:31:35 +0100

    Revert "restauring last version before i commit"
    This reverts commit 75d4b36b30aaa544ae618b7d33e76da244b9119d.

--- cultural_interest.py
+++ cultural_interest.py
@@ -1,20 +1,28 @@
-@@ -4,11 +4,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 df_top10_number = pd.read_csv('./data/6ko_top10_number.csv')
 df_top10_minutes = pd.read_csv('./data/6ko_top10_minutes.csv')
 
-#def display():
-col1, col2 = st.columns(2)
-with col1:
-    st.header("Top 10 in number of media translated")
-    st.dataframe(df_top10_number)
-with col2:
-    st.header("Top 10 in number of minutes translated")
-    st.dataframe(df_top10_minutes)
-def display_international():
-    col1, col2 = st.columns(2)
-    with col1:
-        st.header("Top 10 in number of media translated")
-        st.dataframe(df_top10_number)
-    with col2:
-        st.header("Top 10 in number of minutes translated")
-        st.dataframe(df_top10_minutes)
+df_top10_number['Media number'] = round(df_top10_number['titleId']/1000000,2)
+
+df_top10_minutes['Total duration'] = round(df_top10_minutes['minutes']/60/24)
+
+df_top10 = df_top10_number.merge(df_top10_minutes,how='inner',on='region')
+
+df_top10 = df_top10_number.merge(df_top10_minutes,how='inner',on='region')
+
+
+
+fig, axs = plt.subplots(1,2,figsize=(15, 5))
+
+sns.barplot(data=df_top10,x='Media number', y='region',color='royalblue',ax=axs[0])
+axs[0].set_title('Number of media translated per region')
+
+
+sns.barplot(data=df_top10,x='Total duration', y='region',color='royalblue',ax=axs[1])
+axs[1].set_title('Total duration translated per region in days')
+axs[1].set(ylabel=None)
+
+plt.show()
