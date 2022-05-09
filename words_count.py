007393e6c6f77dbf68a82f68b9baf03a3cec3e57
FMrtnz <fab.mrtnz22@gmail.com>
2022-05-09 11:10:40 +0100

    add custom color wordclouds

--- words_count.py
+++ words_count.py
@@ -5,6 +5,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 from wordcloud import WordCloud
 import numpy as np
 import plotly.express as px
+from PIL import Image
+
+
+def blue_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+    return "hsl(225, 54.55%%, %d%%)" % np.random.uniform(49,59)
+    #return "hsl(200, 100, 50)" % np.random.uniform(60,100)
 
 def display_wordcloud():
     word_freq = pd.read_csv('./data/freq_words_rotten_vs_fresh.csv')
@@ -24,6 +30,7 @@ def display_wordcloud():
     with col1:
         # Set an input to choose region
         genres_explodes = lemmentize_words_genres.explode(["genres"])
+        mask = np.array(Image.open("./img/circle.png"))
         genre_selected = st.selectbox(
              'Select a genre',
              options=list(genres_explodes["genres"].value_counts().index))
@@ -31,5 +38,5 @@ def display_wordcloud():
         vectorizer = CountVectorizer(max_features=10)
         text_matrix = vectorizer.fit_transform(X)
         df_matrix = pd.DataFrame(text_matrix.toarray(), columns = vectorizer.get_feature_names_out())
-        wordcloud = WordCloud(background_color=None, mode="RGBA" ,width=700,height=500, min_font_size=10).generate_from_frequencies(df_matrix.sum())
-        st.image(wordcloud.to_array())
+        wordcloud = WordCloud(mask=mask, background_color=None, mode="RGBA" ,width=700,height=500, min_font_size=10).generate_from_frequencies(df_matrix.sum())
+        st.image(wordcloud.recolor(color_func=blue_color_func, random_state=3).to_array())
