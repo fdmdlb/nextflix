cd2bdb63cb93f0472f26604e71ace905b47a2494
fdmdlb <99870318+fdmdlb@users.noreply.github.com>
2022-05-16 15:17:19 +0100

    color titles and db link

--- interface.py
+++ interface.py
@@ -16,14 +16,12 @@ with st.sidebar:
 
 
 if selected == "Recommendations":
-    st.markdown('<H1 style="color:#ff4b4b;text-align:center;" >NEXTFLIX</H1>', unsafe_allow_html=True)
-    
     ########################################################################
     # Recommendation system
+    df_movies = pd.read_csv('./data/recommendation.csv')
 
-    #df_movies = pd.read_csv('./data/recommendation.csv')
-    df_movies = pd.read_csv('D:/Programmation/nextflix/data/recommendation.csv')
-
+    
+    st.markdown('<H1 style="color:#ff4b4b;text-align:center;" >NEXTFLIX</H1>', unsafe_allow_html=True)
 
     # all-titles-no-accents-lower-case
     df_movies['simple_title'] = df_movies['movie_title'].apply(lambda title: unidecode(title).lower())
@@ -105,7 +103,7 @@ if selected == "Recommendations":
 
 
 if selected == 'About Us':
-    st.markdown('<H1 style="color:red;text-align:center;" >The Team</H1>', unsafe_allow_html=True)
+    st.markdown('<H1 style="color:#ff4b4b;text-align:center;" >The Team</H1>', unsafe_allow_html=True)
 
     linkedin_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">\
                      <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>\
@@ -126,7 +124,7 @@ if selected == 'About Us':
     "link": '<a href="https://www.linkedin.com/in/joana-pires-coelho/">'+linkedin_icon+'</a>'},
     {"name":"Fabien",
      "img": "https://raw.githubusercontent.com/fdmdlb/nextflix/main/img/fabien.png",
-    "link":'<a href="https://www.linkedin.com/in/fabien-martinez-a30561109/">'+linkedin_icon+'</a>'}
+    "link":'<a href="https://linkedin.com/in/fabien-martinez-data-analyst/">'+linkedin_icon+'</a>'}
     ]
     
     st.markdown(
@@ -143,13 +141,13 @@ if selected == 'About Us':
             unsafe_allow_html=True
             )
     st.markdown('<br><br>', unsafe_allow_html=True)
-    st.markdown('<H1 style="color:red;text-align:center;" >The Original Project</H1>', unsafe_allow_html=True)
+    st.markdown('<H1 style="color:#ff4b4b;text-align:center;" >The Original Project</H1>', unsafe_allow_html=True)
     st.markdown("The original project was to perform Exploratory Data Analysis of multiple data sets from IMDb and Rotten Tomatoes web sites, in order to provide a relevent movie recommendation system.<br>You may view the full project on <a href='https://github.com/FMrtnz/project_movie_recommand'>GitHub</a> and <a href='https://share.streamlit.io/fmrtnz/project_movie_recommand/main/interface.py'>Streamlit</a>.", unsafe_allow_html=True)
     
     
     
 if selected == 'Greatest Actor':
-    st.markdown('<H1 style="color:red;text-align:center;" >The Greatest Actor</H1>', unsafe_allow_html=True)    
+    st.markdown('<H1 style="color:#ff4b4b;text-align:center;" >The Greatest Actor</H1>', unsafe_allow_html=True)    
     
     st.markdown(f'<iframe width="700" height="394" src="https://www.youtube-nocookie.com/embed/hp1nTmMdYpE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', unsafe_allow_html=True)
     
